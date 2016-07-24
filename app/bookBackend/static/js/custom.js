c88ed8c8d93610e3187266c6b2946a31df2d66c3
sarthak-04 <4sarthakagrawal@gmail.com>
2016-07-24 17:36:42 +0530

    table values get cleared and new values are added

--- app/bookBackend/static/js/custom.js
+++ app/bookBackend/static/js/custom.js
@@ -1,7 +1,36 @@
-<script>
-  $(document).on('click', '.filters', function() {
-    var val1 = $(this).val($(this).text());
-    alert(val1);
+ $(document).on('click', '.filters', function() {
+    var filterLabel = document.getElementById('filter-value');
+     filterLabel.textContent = this.text;
   });
 
-</script>
+$(document).on('click','#submit-button',function(){
+  // remove the already existing table
+  $("#table tr td").remove();
+  var filterValue = document.getElementById('filter-value').textContent.toLowerCase();
+  var searchTerm = document.getElementById('search-bar').value.toLowerCase();
+  var searchQuery = "/books/?" + filterValue +  "=" + searchTerm;
+  fetch(searchQuery)
+    .then(function(response){
+      if(response.status!==200){
+        console.log("problem with response" + response.status);
+        return;
+      }
+      response.json().then(function(data){
+        if(data){
+          console.log(data);
+          var len = data.results.length;
+          var text = "";
+          if(len>0){
+            for(var i=0;i<len;i++){
+              if(data.results[i].title && data.results[i].author){
+                text +="<tr><td>"+data.results[i].id+"</td><td>"+data.results[i].title+"</td><td>"+data.results[i].author+"</td><td>"+data.results[i].availability+"</td></tr>";
+              }
+            }
+            if(text != ""){
+              $("#table").append(text).removeClass("hidden");
+            }
+          }
+        }
+      });
+    })
+});
